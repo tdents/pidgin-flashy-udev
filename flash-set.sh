c3d74c411c58bd242d9be03d2a69ce7b0422cedb
tdents <tdents@gmail.com>
2015-10-16 03:11:09 +0300

    Configurator for udev rule

---
+++ flash-set.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+dVendor=$(cat /etc/pidgin-flashy.conf | grep 'Vendor' | cut -d '=' -f2 )
+dProduct=$(cat /etc/pidgin-flashy.conf | grep 'Product' | cut -d '=' -f2 )
+devpath=$(ls /sys/bus/usb/devices/*/idVendor | xargs grep -rl $dVendor | awk -F '/idVendor' '{ print $1 "/idProduct" }' | xargs grep -rl $dProduct | awk -F '/idProduct' '{ print $1 }' )
+
+chgrp led $devpath/power/level
+chgrp led $devpath/power/autosuspend_delay_ms
+chgrp led $devpath/power/control
+chgrp led $devpath/power/autosuspend
+chmod "664" $devpath/power/level
+chmod "664" $devpath/power/control
+chmod "664" $devpath/power/autosuspend_delay_ms
+chmod "664" $devpath/power/autosuspend
