3dd4ccf0e84e9b0134501602e19c676994f7c317
tdents <tdents@gmail.com>
2015-10-18 14:53:58 +0300

    Update flash-plugin.pl

--- flash-plugin.pl
+++ flash-plugin.pl
@@ -2,15 +2,15 @@ use Thread qw(yield async);
 use Purple;
 use Time::HiRes qw (sleep);
 
-our $idVendor=`cat /etc/pidgin-flashy.conf | grep -v -e '^#' | grep 'dVendor' | cut -d '=' -f2`;
+our $idVendor=`cat /etc/pidgin-flashy.conf | grep -v '^#' | grep 'dVendor' | cut -d '=' -f2`;
 chomp($idVendor);
-our $idProduct=`cat /etc/pidgin-flashy.conf | grep -v -e '^#' | grep 'dProduct' | cut -d '=' -f2`;
+our $idProduct=`cat /etc/pidgin-flashy.conf | grep -v '^#' | grep 'dProduct' | cut -d '=' -f2`;
 chomp($idProduct);
 
 ###
 %PLUGIN_INFO = (
 	perl_api_version => 2,
-	name => "FlashyLightPlugin",
+	name => "FlashyLightPlugin2",
 	version => "1.0",
 	summary => "Flashing LED connected to USB port",
 	description => "Changes value in /var/flash/status",
@@ -24,43 +24,43 @@ sub plugin_init {
 	return %PLUGIN_INFO;
 }
 sub on {
-        my $devicepath=getdevicepath();
-        	my $devicepowerlevel="$devicepath/power/level";
-		open OUTPUT, ">$devicepowerlevel";
-		print OUTPUT "on";
-		close OUTPUT;
+	Purple::Debug::info("FlashyLightPlugin", "LEDPlugin on device: $devicepowerlevel\n");
+	open OUTPUT, ">$devicepowerlevel";
+	print OUTPUT "on";
+	close OUTPUT;
 }
 
 sub off {
-	my $devicepath=getdevicepath();
-		my $devicepowerlevel="$devicepath/power/level";
-		open OUTPUT, ">$devicepowerlevel";
-        	print OUTPUT "auto";
-        	close OUTPUT;
+	Purple::Debug::info("FlashyLightPlugin", "LEDPlugin off device: $devicepowerlevel\n");
+        open OUTPUT, ">$devicepowerlevel";
+        print OUTPUT "auto";
+        close OUTPUT;
 }
 
-sub getdevicepath {
-        our $devicepath=`udevadm trigger -v -a idVendor=$idVendor -a idProduct=$idProduct`;
-        chomp($devicepath);
-	return $devicepath;
-}
+sub startflash {
+	async {
+		our $devicepath=`udevadm trigger -v -a idVendor=$idVendor -a idProduct=$idProduct`;
+		chomp($devicepath);
+		Purple::Debug::info("FlashyLightPlugin", "LEDPlugin async start device: $devicepath\n");
+		if(!$devicepath == '')
+		{
+			our $devicepowerlevel="$devicepath/power/level";
+			for ($exitapp; total_unread_count() > 0; $exitapp)
+			{
+				$unread = total_unread_count();
+				Purple::Debug::info("FlashyLightPlugin", "LEDPlugin cycle: $unread\n");
+				on();
+				Time::HiRes::sleep (0.5);
+				off();
+				Time::HiRes::sleep (0.5);
+		        }
+		}	
+		Purple::Debug::info("FlashyLightPlugin", "LEDPlugin exit cycle\n");
+	}
+};
 
 sub plugin_load {
-	my $plugin = shift;
-	
-	$t=async {
-		for ($exitapp; $exitapp > -1; $exitapp)
-		{
-			Time::HiRes::sleep (0.1);
-			my $unread = total_unread_count();
-			if(total_unread_count() > 0) {
-			on();
-			Time::HiRes::sleep (0.5);
-			off();
-			Time::HiRes::sleep (0.5);
-                }
-        } 
-};
+        my $plugin = shift;
 
 	my $conv_handle = Purple::Conversations::get_handle();
 	
@@ -69,7 +69,6 @@ sub plugin_load {
 	Purple::Signal::connect($conv_handle, "conversation-updated", $plugin, \&update_unread_count, '');
 	Purple::Signal::connect($conv_handle, "conversation-created", $plugin, \&update_unread_count, '');
 	Purple::Signal::connect($conv_handle, "deleting-conversation", $plugin, \&update_unread_count, '');
-	off();
 }
 
 sub total_unread_count {
@@ -87,6 +86,7 @@ sub total_unread_count {
 
 sub update_unread_count {
 	my $unread = total_unread_count();
+	if($unread > 0) { startflash(); };
 }
 
 sub plugin_unload {
